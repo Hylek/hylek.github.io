a4261076cd3e7248b2411635a10b995d09600538
Daniel Cumbor <dcdolphy@gmail.com>
2021-01-08 00:35:08 +0000

    Added Navbar and Button Components

---
+++ src/components/Navbar.js
@@ -0,0 +1,43 @@
+import React, {useState} from 'react'
+import { Link } from 'react-router-dom'
+
+function Navbar() {
+    const [click, setClick] = useState(false);
+
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+
+    return (
+        <>
+            <nav className="navbar">
+                <div className="navbar-container">
+                    <Link to="/" className="navbar-logo">
+                        wot
+                    </Link>
+                    <div className='menu-icon' onClick={ handleClick }>
+                        <i className= { click ? 'fas fa-times' : 'fas fa-bars'} />
+                    </div>
+                    <ul className={ click ? 'nav-menu active' : 'nav-menu' }>
+                        <li className='nav-item'>
+                            <Link to='/' className='nav-lins' onClick={ closeMobileMenu }>
+                                Home
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/about' className='nav-lins' onClick={ closeMobileMenu }>
+                                About
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/contact' className='nav-lins' onClick={ closeMobileMenu }>
+                                Contact
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar
